b8c50e6ac753f5dc66c669866fb664cc78a45604
刘威 <liuwei@yiqiandai.com>
2019-12-13 14:33:12 +0800

    初始化sql

---
+++ doc/init.sql
@@ -0,0 +1,28 @@
+CREATE TABLE `persistent_logins` (
+  `username` varchar(64) NOT NULL,
+  `series` varchar(64) NOT NULL,
+  `token` varchar(64) NOT NULL,
+  `last_used` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`series`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+-- This SQL contains a "create table" that can be used to create a table that JdbcUsersConnectionRepository can persist
+-- connection in. It is, however, not to be assumed to be production-ready, all-purpose SQL. It is merely representative
+-- of the kind of table that JdbcUsersConnectionRepository works with. The table and column names, as well as the general
+-- column types, are what is important. Specific column types and sizes that work may vary across database vendors and
+-- the required sizes may vary across API providers. 
+
+create table UserConnection (userId varchar(255) not null,
+	providerId varchar(255) not null,
+	providerUserId varchar(255),
+	rank int not null,
+	displayName varchar(255),
+	profileUrl varchar(512),
+	imageUrl varchar(512),
+	accessToken varchar(512) not null,
+	secret varchar(512),
+	refreshToken varchar(512),
+	expireTime bigint,
+	primary key (userId, providerId, providerUserId));
+create unique index UserConnectionRank on UserConnection(userId, providerId, rank);
