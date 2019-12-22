de530f495ef79522456cea963318e21383844b99
刘威 <liuwei@yiqiandai.com>
2019-12-22 17:14:24 +0800

    oath2 配置 改成java8的写法

--- security-app/src/main/java/com/my/security/authorization/AuthenticationServerConfig.java
+++ security-app/src/main/java/com/my/security/authorization/AuthenticationServerConfig.java
@@ -1,14 +1,23 @@
 package com.my.security.authorization;
 
+import java.util.Arrays;
+
+import org.apache.commons.lang.ArrayUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.builders.InMemoryClientDetailsServiceBuilder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+
+import com.my.security.properites.ClientInfoProperties;
+import com.my.security.properites.SecurityProperties;
+
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * 认证服务器配置
@@ -17,6 +26,7 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
  */
 @Configuration
 @EnableAuthorizationServer
+@Slf4j
 public class AuthenticationServerConfig extends AuthorizationServerConfigurerAdapter {
 //
 	@Autowired
@@ -25,6 +35,8 @@ public class AuthenticationServerConfig extends AuthorizationServerConfigurerAda
 	private AuthenticationManager authenticationManager;
 	@Autowired
 	private UserDetailsService userDetailsService;
+	@Autowired
+	private SecurityProperties securityProperties;
 	
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
@@ -43,14 +55,24 @@ public class AuthenticationServerConfig extends AuthorizationServerConfigurerAda
 //	
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-		  clients.inMemory() // 使用in-memory存储
-          .withClient("myDemoClient") // client_id
-          .secret(passwordEncoder.encode("myDemoSecret")) // client_secret
-          .authorizedGrantTypes("authorization_code","password","refresh_token") // 该client允许的授权类型
-          .redirectUris("http://example.com")
-          .scopes("all"); // 允许的授权范围
+		
+		InMemoryClientDetailsServiceBuilder builder =  clients.inMemory(); // 使用in-memory存储
+		if(ArrayUtils.isNotEmpty(securityProperties.getOauth2().getClients())) {
+			Arrays.stream(securityProperties.getOauth2().getClients()).forEach(e->buildMerory(e,builder));
+		}else {
+			log.error("outh2 get client info Nothing");
+			
+		}
 	}
 	
+	private void buildMerory(ClientInfoProperties e, InMemoryClientDetailsServiceBuilder builder) {
+		   builder.withClient(e.getClientId()) // client_id
+	          .secret(passwordEncoder.encode(e.getClientSecert())) // client_secret
+	          .authorizedGrantTypes("authorization_code","password","refresh_token") // 该client允许的授权类型
+	          .accessTokenValiditySeconds(e.getAccessTokenValiditySeconds())
+	          .redirectUris(e.getRedirectUris())
+	          .scopes("all"); // 允许的授权范围
+	}
 	
 	
 
