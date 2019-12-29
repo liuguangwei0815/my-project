ea0c46333a7fc63d8bacb3377729a9f54648f56f
lw <736873792@qq.com>
2019-12-29 22:38:34 +0800

    1、zuul 转发认证服务和资源服务转发配置

--- security-api-getway/src/main/java/com/my/security/SecurityApiGetWayApplication.java
+++ security-api-getway/src/main/java/com/my/security/SecurityApiGetWayApplication.java
@@ -2,8 +2,10 @@ package com.my.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy//开启网关
 public class SecurityApiGetWayApplication {
 	
 	public static void main(String[] args) {
